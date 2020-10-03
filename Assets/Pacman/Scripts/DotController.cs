ed250a7c26d399b93ae0f9d4cfe7ffeef7d9b9af
Tanner Dyer <tdyer@hawk.iit.edu>
2020-10-03 14:43:18 -0500

    populated maze with pacdots

---
+++ Assets/Pacman/Scripts/DotController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotController : MonoBehaviour
+{
+    // on collision trigger
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.name == "pacman")
+            Destroy(gameObject);
+    }
+}
--- Assets/Pacman/Scripts/PacmanController.cs
+++ Assets/Pacman/Scripts/PacmanController.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pacman : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PacmanController : MonoBehaviour {
+    public float speed = 0.4f;
+    Vector2 destination = Vector2.zero;
+
+    void Start () {
+        destination = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate () {
+        // gradualy move towards destination
+        Vector2 path = Vector2.MoveTowards(transform.position, destination, speed);
+        GetComponent<Rigidbody2D>().MovePosition(path);
+
+        // checking user input
+        if ((Vector2) transform.position == destination) {
+            if (Input.GetKey(KeyCode.UpArrow) && canMove(Vector2.up))
+                destination = (Vector2)transform.position + Vector2.up;
+            if (Input.GetKey(KeyCode.RightArrow) && canMove(-Vector2.right))
+                destination = (Vector2)transform.position - Vector2.right;
+            if (Input.GetKey(KeyCode.DownArrow) && canMove(-Vector2.up))
+                destination = (Vector2)transform.position - Vector2.up;
+            if (Input.GetKey(KeyCode.LeftArrow) && canMove(Vector2.right))
+                destination = (Vector2)transform.position + Vector2.right;
+        }
+
+        // TODO: Fix this weird animation error
+        // animations
+        // Vector2 dir = destination - (Vector2)transform.position;
+        // GetComponent<Animator>().setFloat("XDirection", dir.x);
+        // GetComponent<Animator>().setFloat("YDirection", dir.y);
+    }
+
+    bool canMove (Vector2 dir) {
+        // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+        return (hit.collider == GetComponent<Collider2D>());
     }
 }

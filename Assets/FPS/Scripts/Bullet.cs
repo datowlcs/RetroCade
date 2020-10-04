6ca34b08200dda94562949043cec9b34e53c5972
datowlcs <datowlcs@gmail.com>
2020-10-04 06:26:44 -0700

    CovidReliefGame Branch
    All Assets/Game logic with CovidRelief game. Includes working navmesh

---
+++ Assets/FPS/Scripts/Bullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Bullet : MonoBehaviour
+{
+
+    public Rigidbody rbody;
+
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Activate(Vector3 position, Vector3 velocity)
+    {
+        transform.position = position;
+        rbody.velocity = velocity;
+
+        gameObject.SetActive(true);
+
+        StartCoroutine("Decay");
+    }
+
+    private IEnumerator Decay()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        Deactivate();
+    }
+
+    public void Deactivate()
+    {
+        BulletPool.main.AddToPool(this);
+
+        StopAllCoroutines();
+
+        gameObject.SetActive(false);
+    }
+
+    //This is where we handle what happens when a bullet hits an enemy
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("We hit something");
+
+        if (other.gameObject.tag == "Germ")
+        {
+            Destroy(other.gameObject);
+
+            Deactivate();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/FPS/Scripts/BulletPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BulletPool : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //gets the main bulletpool of the scene
+    public static BulletPool main;
+
+    public GameObject bulletPrefab;
+    public int poolSize = 100;
+
+    private List<Bullet> availableBullets;
+
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    void Start()
+    {
+        availableBullets = new List<Bullet>();
+
+        for(int i=0; i<poolSize; i++)
+        {
+            Bullet b = Instantiate(bulletPrefab, transform).GetComponent<Bullet>();
+            b.gameObject.SetActive(false);
+
+            availableBullets.Add(b);
+        }
+
+    }
+
+    public void PickFromPool(Vector3 position, Vector3 velocity)
+    {
+        if (availableBullets.Count < 1) return;
+
+        availableBullets[0].Activate(position, velocity);
+
+        availableBullets.RemoveAt(0);
+    }
+
+    public void AddToPool(Bullet bullet)
+    {
+        if (!availableBullets.Contains(bullet)) availableBullets.Add(bullet);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
